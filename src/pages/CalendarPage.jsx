438c5438b60ad2916d7865963ccd716db647897f
abizmo <abizmo@gmail.com>
2021-10-12 21:48:47 +0100

    Add Calendar View

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,10 +1,27 @@
+import moment from 'moment';
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 import Navbar from '../components/ui/Navbar';
+import '../styles/calendar.css';
+
+const events = [
+  {
+    id: 0,
+    title: 'First event',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours'),
+  },
+];
 
 const CalendarPage = () => (
-  <div>
+  <div className="calendar-container">
     <Navbar />
-    CalendarPage
+    <Calendar
+      events={events}
+      localizer={momentLocalizer(moment)}
+    />
   </div>
 );
 
