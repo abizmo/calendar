e470b051c957f3bb3d8cb8b46f4f0efc2aebf97b
abizmo <abizmo@gmail.com>
2021-10-13 06:41:13 +0100

    Add spanish messages and demo style

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,10 +1,16 @@
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 import moment from 'moment';
 import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import Navbar from '../components/ui/Navbar';
 import '../styles/calendar.css';
+import messages from '../localization/calendar-messages-es';
+
+moment.locale('es');
 
 const events = [
   {
@@ -15,12 +21,22 @@ const events = [
   },
 ];
 
+const eventStyleGetter = (_event, _start, _end, _isSelected) => ({
+  style: {
+    backgroundColor: 'red',
+    borderRadius: '0px',
+    color: 'white',
+  },
+});
+
 const CalendarPage = () => (
   <div className="calendar-container">
     <Navbar />
     <Calendar
       events={events}
+      eventPropGetter={eventStyleGetter}
       localizer={momentLocalizer(moment)}
+      messages={messages}
     />
   </div>
 );
